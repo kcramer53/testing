918d90497f2fff55d300c81addc87fe4a419876f
Student <example@example.com>
2019-06-19 19:50:49 -0700

    resubmission

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,42 @@
 
 
 	$(document).ready(function() {
+		// when user submitsm it will determine what is inputted and outputted
 	  $("form").submit(function(event) {
+			//next few lines will gather and save into variable whatever the user inputs
 	    var favoritelanguage = $("#language").val();
+			console.log("favoriteLanguage " + favoritelanguage);
       var favoriteVacation=$("input:radio[name=vacation]:checked").val();
-      var favoriteVacation=$("input:radio[name=vacation]:checked").val();
+			console.log("favoriteVacation " + favoriteVacation);
 	    var favoriteCusine = $("input:radio[name=cusine]:checked").val();
+			console.log("favoriteCusine " + favoriteCusine);
 	    var favoriteAccomodations = $("input:radio[name=accomodations]:checked").val();
+			console.log("favoriteAccomodations " + favoriteAccomodations);
 	    var favoriteApproach = $("input:radio[name=approach]:checked").val();
+			console.log("favoriteApproach " + favoriteApproach);
 	    var favoriteRandom = $("input:radio[name=random]:checked").val();
-			console.log(language);
+			console.log("favoriteApproach " + favoriteApproach);
 
 
+//these ifs evaluate the users input to determine what to display
 	    if (favoriteVacation === "tropics" && favoriteCusine === "seafood" && favoriteAccomodations === "youth hostel" && favoriteApproach === "visual concept" && favoriteRandom === "i am always on time") {
 	      $('#ruby').show();
-	      $('#java, #plusplus, #python, #no-match').hide();
-	    } else if (favoriteVacation === "mountians" && favoriteCusine === "exotic" && favoriteAccomodations === "all inclusive" && favoriteApproach === "demonstration" && favoriteRandom === "i am the life of the party") {
+	      $('#java, #cplusplus, #python, #no-match').hide();
+	    } else if (favoriteVacation === "mountains" && favoriteCusine === "exotic" && favoriteAccomodations === "all inclusive" && favoriteApproach === "demonstration" && favoriteRandom === "i am the life of the party") {
         $('#java').show();
-        $('#ruby, #plusplus, #python, #no-match').hide();
+        $('#ruby, #cplusplus, #python, #no-match').hide();
 	    } else if (favoriteVacation === "dessert" && favoriteCusine === "traditional" && favoriteAccomodations === "hotel" && favoriteApproach === "online" && favoriteRandom === "i never give up on true love") {
-        $('#plusplus').show();
+        $('#cplusplus').show();
         $('#java, #ruby, #python, #no-match').hide();
-	    } else if (favoriteVacation === "stacation" && favoriteCusine === "meat & taters" && favoriteAccomodations === "tent" && favoriteApproach === "classroom" && favoriteRandom === "i will be rich some day") {
+	    } else if (favoriteVacation === "staycation" && favoriteCusine === "meat&taters" && favoriteAccomodations === "tent" && favoriteApproach === "classroom" && favoriteRandom === "i will be rich one day") {
         $('#python').show();
-        $('#java, #plusplus, #ruby, #no-match').hide();
+        $('#java, #cplusplus, #ruby, #no-match').hide();
 	    } else {
         $('#no-match').show();
-        $('#java, #plusplus, #python, #ruby').hide();
+        $('#java, #cplusplus, #python, #ruby').hide();
 	    }
 
-
+//this prevents refresh after submit is clicked
 	  event.preventDefault();
 	  });
 	});
