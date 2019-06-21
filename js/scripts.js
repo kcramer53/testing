3db03c2cf1b71eba74ee9476a48748a6fcfec0ec
kcramer53 <51129734+kcramer53@users.noreply.github.com>
2019-06-21 11:13:32 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // business logic
-$(document).ready(function() {
+	$(document).ready(function() {
 		// when user submitsm it will determine what is inputted and outputted
 	  $("form").submit(function(event) {
 			//next few lines will gather and save into variable whatever the user inputs
 	    var favoritelanguage = $("#language").val();
 			console.log("favoriteLanguage " + favoritelanguage);
-      var favoriteVacation=$("input:radio[name=vacation]:checked").val();
+            var favoriteVacation=$("input:radio[name=vacation]:checked").val();
 			console.log("favoriteVacation " + favoriteVacation);
 	    var favoriteCusine = $("input:radio[name=cusine]:checked").val();
 			console.log("favoriteCusine " + favoriteCusine);
@@ -21,17 +21,17 @@ $(document).ready(function() {
 	      $('#ruby').show();
 	      $('#java, #cplusplus, #python, #no-match').hide();
 	    } else if (favoriteVacation === "mountains" && favoriteCusine === "exotic" && favoriteAccomodations === "all inclusive" && favoriteApproach === "demonstration" && favoriteRandom === "i am the life of the party") {
-        $('#java').show();
-        $('#ruby, #cplusplus, #python, #no-match').hide();
+        	$('#java').show();
+        	$('#ruby, #cplusplus, #python, #no-match').hide();
 	    } else if (favoriteVacation === "dessert" && favoriteCusine === "traditional" && favoriteAccomodations === "hotel" && favoriteApproach === "online" && favoriteRandom === "i never give up on true love") {
-        $('#cplusplus').show();
-        $('#java, #ruby, #python, #no-match').hide();
+        	$('#cplusplus').show();
+        	$('#java, #ruby, #python, #no-match').hide();
 	    } else if (favoriteVacation === "staycation" && favoriteCusine === "meat&taters" && favoriteAccomodations === "tent" && favoriteApproach === "classroom" && favoriteRandom === "i will be rich one day") {
-        $('#python').show();
-        $('#java, #cplusplus, #ruby, #no-match').hide();
+        	$('#python').show();
+        	$('#java, #cplusplus, #ruby, #no-match').hide();
 	    } else {
-        $('#no-match').show();
-        $('#java, #cplusplus, #python, #ruby').hide();
+        	$('#no-match').show();
+        	$('#java, #cplusplus, #python, #ruby').hide();
 	    }
 //this prevents refresh after submit is clicked
 	  event.preventDefault();
